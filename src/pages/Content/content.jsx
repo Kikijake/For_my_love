71231de64a00534353c9d39ec1580572989cbc32
yehtetsan1 <yehtetsan.zotefamily@gmail.com>
2025-02-12 22:03:47 +0630

    fix

--- src/pages/Content/content.jsx
+++ src/pages/Content/content.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from "react";
+import React, { useEffect, useState } from "react";
 import phoo from "../../assets/photos/phoo.png";
 import yePhoo from "../../assets/photos/Ye_Phoo.png";
 // lottie
--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -1,12 +1,4 @@
 import React, { useState } from "react";
-// lottie
-import Lottie from "lottie-react";
-// animations
-import smallHeartBg from "../../assets/animations/small-heart-bg.json";
-// font-awesome-component
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// font-awesome-icon
-import { faHeart } from "@fortawesome/free-solid-svg-icons";
 // Page Component
 import HomeBanner from "../../components/Home/homeBanner";
 import Content from "../Content/content";
