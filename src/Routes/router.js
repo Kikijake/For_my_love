596b2459ff329bc057a3b74f70da539bcbb5af4b
yehtetsan1 <yehtetsan.zotefamily@gmail.com>
2025-02-12 23:25:01 +0630

    test

--- src/Routes/router.js
+++ src/Routes/router.js
@@ -5,8 +5,8 @@ import { Home, Quiz } from "./pages";
 const Router = () => {
   return (
     <Routes>
-      <Route path="*" element={<Navigate to="/" />} />
-      <Route path="/" name="homeScreen" element={<Home />} />
+      {/* <Route path="*" element={<Navigate to="/" />} /> */}
+      <Route path="*" name="homeScreen" element={<Home />} />
       {/* <Route path="/quiz" element={<Quiz />} /> */}
     </Routes>
   );
